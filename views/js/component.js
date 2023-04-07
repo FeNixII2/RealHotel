6546c65cf2bdbf7080aef33681550377958eabad
FeNixll2 <fiw24018@gmail.com>
2023-04-07 19:23:52 +0700

    อัพเดต

--- views/js/component.js
+++ views/js/component.js
@@ -5,19 +5,23 @@ class Footer extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-        
-        <div class="container">
+
+    <div class="container">
         <footer class="py-3 my-4">
-        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
-          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
-          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
-        </ul>
-        <p class="text-center text-muted">© 2022 Company, Inc</p>
+          <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item"><a href="#home" onclick="showSection('home')" class="nav-link px-2 text-muted">หน้าหลัก</a></li>
+            <li class="nav-item"><a href="#rooms" onclick="showSection('rooms')" class="nav-link px-2 text-muted">ห้องพัก</a></li>
+            <li class="nav-item"><a href="#booking" onclick="showSection('booking'),booking_search()" class="nav-link px-2 text-muted">จองห้องพัก</a></li>
+            <li class="nav-item">
+              <a href="#" class="nav-link px-2 text-muted"><i class="fa-brands fa-facebook"></i></a>
+            </li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted"><i class="fa-brands fa-youtube"></i></a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">ติดต่อ 0123456789</a></li>
+
+          </ul>
+          <p class="text-center text-muted">© 2023 โรงแรมบีเอส</p>
         </footer>
-        </div>
+      </div>
         `;
   }
 }
