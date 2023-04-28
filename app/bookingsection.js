56f8f90eba9f9a720f1d32cdb4aa899c9792bb67
mild <boommild1@gmail.com>
2023-04-28 18:43:40 +0700

    asdasd

--- app/bookingsection.js
+++ app/bookingsection.js
@@ -1,63 +1,64 @@
 module.exports = function(app, con, moment, transporter) {
-        app.post("/booking_search", (req, res) => {
-            var { checkin, checkout, roomtype } = req.body;
-            if (roomtype != 'all') {
-                con.query("SELECT COUNT(*) as count_available_rooms FROM rooms WHERE id_typeroom = ? AND num_room NOT IN (SELECT num_room FROM reserved WHERE id_typeroom = ? AND (checkin BETWEEN ? AND ? OR checkout BETWEEN ? AND ? OR (checkin <= ? AND checkout >= ?)))", [roomtype, roomtype, checkin, checkout, checkin, checkout, checkin, checkout], (err, results) => {
-                    if (err) throw err;
-                    const count_available_rooms = results[0].count_available_rooms;
-                    con.query("select * from roomstype WHERE id = ? order by price asc", [roomtype], (err, roomtype) => {
-                        if (err) throw err
-                        if (count_available_rooms != 0) {
-                            res.send({ success: true, count_available_rooms, roomtype })
-                        } else if (count_available_rooms == 0) {
-                            res.send({ success: false, count_available_rooms, roomtype })
-                        }
-                    })
-                });
-
-            } else if (roomtype == 'all') {
-                con.query("SELECT COUNT(*) as count_available_rooms, id_typeroom FROM rooms WHERE num_room NOT IN (SELECT num_room FROM reserved WHERE (checkin BETWEEN ? AND ? OR checkout BETWEEN ? AND ? OR (checkin <= ? AND checkout >= ?))) GROUP BY id_typeroom", [checkin, checkout, checkin, checkout, checkin, checkout], (err, results) => {
-                    if (err) throw err;
-                    con.query("select * from roomstype order by price asc", (err, roomtype) => {
-                        const countAvailableRooms = results.reduce((acc, curr) => acc + curr.count_available_rooms, 0);
-                        const success = countAvailableRooms > 0;
-                        res.send({ success, count_available_rooms: results, roomtype });
-                    })
-                });
-            }
-        })
-
-
-        app.post('/confirm_booking_room', (req, res) => {
-            var { firstName, p_number, email, lastName, more_info, payment, checkin, checkout, room_type } = req.body
-            con.query("select id from customer where f_name = ? and l_name = ? and p_num = ? and email = ?", [firstName, lastName, p_number, email], (err, cus_id) => {
-                if (err) throw err
-                if (cus_id.length != 0) {
-                    reserv(more_info, payment, checkin, checkout, room_type, cus_id[0].id)
-                    res.send({})
-                } else if (cus_id.length == 0) {
-                    con.query("insert into customer values ('',?,?,?,?)", [firstName, lastName, p_number, email], (err, result) => {
-                        if (err) throw err
-                        con.query("select id from customer where f_name = ? and l_name = ? and p_num = ? and email = ?", [firstName, lastName, p_number, email], (err, cus_id) => {
-                            if (err) throw err
-                            reserv(more_info, payment, checkin, checkout, room_type, cus_id[0].id)
-                            res.send({})
-                        })
-                    })
-                }
-            })
-        })
-
-
-        function reserv(more_info, payment, checkin, checkout, room_type, cus_id) {
-            con.query(" SELECT num_room FROM rooms WHERE id_typeroom = ? AND num_room NOT IN ( SELECT num_room  FROM reserved WHERE id_typeroom = ? AND (checkin BETWEEN ? AND ? OR checkout BETWEEN ? AND ? OR (checkin <= ? AND checkout >= ?))) LIMIT 1 ", [room_type, room_type, checkin, checkout, checkin, checkout, checkin, checkout], (err, num_room) => {
-                if (err) throw err
-                con.query("insert into reserved values ('',?,?,?,?,'0',?,?,?) ", [num_room[0].num_room, room_type, checkin, checkout, cus_id, more_info, payment], (err, result) => {
+    app.post("/booking_search", (req, res) => {
+        var { checkin, checkout, roomtype } = req.body;
+        if (roomtype != 'all') {
+            con.query("SELECT COUNT(*) as count_available_rooms FROM rooms WHERE id_typeroom = ? AND num_room NOT IN (SELECT num_room FROM reserved WHERE id_typeroom = ? AND (checkin BETWEEN ? AND ? OR checkout BETWEEN ? AND ? OR (checkin <= ? AND checkout >= ?)))", [roomtype, roomtype, checkin, checkout, checkin, checkout, checkin, checkout], (err, results) => {
+                if (err) throw err;
+                const count_available_rooms = results[0].count_available_rooms;
+                con.query("select * from roomstype WHERE id = ? order by price asc", [roomtype], (err, roomtype) => {
                     if (err) throw err
-                    var detail = 'ได้ทำการชำระเงินสำหรับห้อง ' + num_room[0].num_room + ' แล้ว'
-                    con.query("insert into payment_log values ('',?,?) ", [detail, cus_id], (err, result) => {
+                    if (count_available_rooms != 0) {
+                        res.send({ success: true, count_available_rooms, roomtype })
+                    } else if (count_available_rooms == 0) {
+                        res.send({ success: false, count_available_rooms, roomtype })
+                    }
+                })
+            });
+
+        } else if (roomtype == 'all') {
+            con.query("SELECT COUNT(*) as count_available_rooms, id_typeroom FROM rooms WHERE num_room NOT IN (SELECT num_room FROM reserved WHERE (checkin BETWEEN ? AND ? OR checkout BETWEEN ? AND ? OR (checkin <= ? AND checkout >= ?))) GROUP BY id_typeroom", [checkin, checkout, checkin, checkout, checkin, checkout], (err, results) => {
+                if (err) throw err;
+                con.query("select * from roomstype order by price asc", (err, roomtype) => {
+                    const countAvailableRooms = results.reduce((acc, curr) => acc + curr.count_available_rooms, 0);
+                    const success = countAvailableRooms > 0;
+                    res.send({ success, count_available_rooms: results, roomtype });
+                })
+            });
+        }
+    })
+
+
+    app.post('/confirm_booking_room', (req, res) => {
+        var { firstName, p_number, email, lastName, more_info, payment, checkin, checkout, room_type } = req.body
+        con.query("select id from customer where f_name = ? and l_name = ? and p_num = ? and email = ?", [firstName, lastName, p_number, email], (err, cus_id) => {
+            if (err) throw err
+            if (cus_id.length != 0) {
+                reserv(more_info, payment, checkin, checkout, room_type, cus_id[0].id)
+                res.send({})
+            } else if (cus_id.length == 0) {
+                con.query("insert into customer values ('',?,?,?,?)", [firstName, lastName, p_number, email], (err, result) => {
+                    if (err) throw err
+                    con.query("select id from customer where f_name = ? and l_name = ? and p_num = ? and email = ?", [firstName, lastName, p_number, email], (err, cus_id) => {
                         if (err) throw err
+                        reserv(more_info, payment, checkin, checkout, room_type, cus_id[0].id)
+                        res.send({})
                     })
                 })
+            }
+        })
+    })
+
+
+    function reserv(more_info, payment, checkin, checkout, room_type, cus_id) {
+        con.query(" SELECT num_room FROM rooms WHERE id_typeroom = ? AND num_room NOT IN ( SELECT num_room  FROM reserved WHERE id_typeroom = ? AND (checkin BETWEEN ? AND ? OR checkout BETWEEN ? AND ? OR (checkin <= ? AND checkout >= ?))) LIMIT 1 ", [room_type, room_type, checkin, checkout, checkin, checkout, checkin, checkout], (err, num_room) => {
+            if (err) throw err
+            con.query("insert into reserved values ('',?,?,?,?,'0',?,?,?) ", [num_room[0].num_room, room_type, checkin, checkout, cus_id, more_info, payment], (err, result) => {
+                if (err) throw err
+                var detail = 'ได้ทำการชำระเงินสำหรับห้อง ' + num_room[0].num_room + ' แล้ว'
+                con.query("insert into payment_log values ('',?,?) ", [detail, cus_id], (err, result) => {
+                    if (err) throw err
+                })
             })
-        }
+        })
+    }
+}
