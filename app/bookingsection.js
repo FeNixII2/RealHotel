d0ec800fa7ae32707d12bc4d4a9ee1db1f7b5d67
mild <boommild1@gmail.com>
2023-04-28 17:58:17 +0700

    เพิ่ม รหัสดูรายละเอียด

--- app/bookingsection.js
+++ app/bookingsection.js
@@ -1,4 +1,4 @@
-module.exports = function (app, con, moment, transporter) {
+module.exports = function(app, con, moment, transporter) {
     app.post("/booking_search", (req, res) => {
         var { checkin, checkout, roomtype } = req.body;
         if (roomtype != 'all') {
@@ -33,32 +33,38 @@ module.exports = function (app, con, moment, transporter) {
         con.query("select id from customer where f_name = ? and l_name = ? and p_num = ? and email = ?", [firstName, lastName, p_number, email], (err, cus_id) => {
             if (err) throw err
             if (cus_id.length != 0) {
-                reserv(more_info, payment, checkin, checkout, room_type, cus_id[0].id)
+                reserv(more_info, payment, checkin, checkout, room_type, cus_id[0].id, function(reserved_custom_id) {
+                    console.log("Reserved custom ID:", reserved_custom_id);
+                    res.send({ reserved_custom_id })
+                });
                 res.send({})
             } else if (cus_id.length == 0) {
-                con.query("insert into customer values ('',?,?,?,?)", [firstName, lastName, p_number, email], (err, result) => {
-                    // result.insertId
+                con.query("insert into customer values ('',?,?,?,?)", [firstName, lastName, p_number, email], (err, cus_id) => {
+                    var cus_id = cus_id.insertId
                     if (err) throw err
-                    con.query("select id from customer where f_name = ? and l_name = ? and p_num = ? and email = ?", [firstName, lastName, p_number, email], (err, cus_id) => {
-                        if (err) throw err
-                        reserv(more_info, payment, checkin, checkout, room_type, cus_id[0].id)
-                        res.send({})
-                    })
+                    reserv(more_info, payment, checkin, checkout, room_type, cus_id, function(reserved_custom_id) {
+                        console.log("Reserved custom ID:", reserved_custom_id);
+                        res.send({ reserved_custom_id })
+                    });
+
                 })
             }
         })
     })
 
-    function reserv(more_info, payment, checkin, checkout, room_type, cus_id) {
+    function reserv(more_info, payment, checkin, checkout, room_type, cus_id, callback) {
         const currentDate = moment();
         const formattedDate = currentDate.format('DD/MM/YYYY HH:mm');
         con.query(" SELECT num_room FROM rooms WHERE id_typeroom = ? AND num_room NOT IN ( SELECT num_room  FROM reserved WHERE id_typeroom = ? AND (checkin BETWEEN ? AND ? OR checkout BETWEEN ? AND ? OR (checkin <= ? AND checkout >= ?))) LIMIT 1 ", [room_type, room_type, checkin, checkout, checkin, checkout, checkin, checkout], (err, num_room) => {
             if (err) throw err
-            con.query("insert into reserved values ('',?,?,?,?,'0',?,?,?) ", [num_room[0].num_room, room_type, checkin, checkout, cus_id, more_info, payment], (err, result) => {
+            var reserved_custom_date = currentDate.format('DDMMYYHHmm')
+            var reserved_custom_id = 'SF' + cus_id + reserved_custom_date + num_room[0].num_room
+            con.query("insert into reserved (num_room,id_typeroom,checkin,checkout,cus_id, more_info, payment,reserved_id,status) values (?,?,?,?,?,?,?,?,0) ", [num_room[0].num_room, room_type, checkin, checkout, cus_id, more_info, payment, reserved_custom_id], (err, result) => {
                 if (err) throw err
                 var detail = 'ได้ทำการชำระเงินสำหรับห้อง ' + num_room[0].num_room + ' แล้ว'
                 con.query("insert into payment_log values ('',?,?,?) ", [detail, cus_id, formattedDate], (err, result) => {
                     if (err) throw err
+                    callback(reserved_custom_id);
                 })
             })
         })
