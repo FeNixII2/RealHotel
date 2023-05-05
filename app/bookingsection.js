15be8357c914afd6674cf36a6dda4d801b1e5051
mild <boommild1@gmail.com>
2023-05-05 14:14:32 +0700

    dee mak

--- app/bookingsection.js
+++ app/bookingsection.js
@@ -2,7 +2,7 @@ module.exports = function (app, con, moment, transporter) {
     app.post("/booking_search", (req, res) => {
         var { checkin, checkout, roomtype } = req.body;
         if (roomtype != 'all') {
-            con.query("SELECT COUNT(*) as count_available_rooms FROM rooms WHERE id_typeroom = ? AND num_room NOT IN (SELECT num_room FROM reserved WHERE id_typeroom = ? AND (checkin BETWEEN ? AND ? OR checkout BETWEEN ? AND ? OR (checkin <= ? AND checkout >= ?))) ", [roomtype, roomtype, checkin, checkout, checkin, checkout, checkin, checkout], (err, results) => {
+            con.query("SELECT COUNT(*) as count_available_rooms FROM rooms WHERE id_typeroom = ? AND num_room NOT IN (SELECT num_room FROM reserved WHERE id_typeroom = ? AND (checkin BETWEEN ? AND ? OR checkout BETWEEN ? AND ? OR (checkin <= ? AND checkout >= ?))and status != '5') ", [roomtype, roomtype, checkin, checkout, checkin, checkout, checkin, checkout], (err, results) => {
                 if (err) throw err;
                 const count_available_rooms = results[0].count_available_rooms;
                 con.query("select * from roomstype WHERE id = ? order by price asc", [roomtype], (err, roomtype) => {
@@ -16,7 +16,7 @@ module.exports = function (app, con, moment, transporter) {
             });
 
         } else if (roomtype == 'all') {
-            con.query("SELECT COUNT(*) as count_available_rooms, id_typeroom FROM rooms WHERE num_room NOT IN (SELECT num_room FROM reserved WHERE (checkin BETWEEN ? AND ? OR checkout BETWEEN ? AND ? OR (checkin <= ? AND checkout >= ?))) GROUP BY id_typeroom", [checkin, checkout, checkin, checkout, checkin, checkout], (err, results) => {
+            con.query("SELECT COUNT(*) as count_available_rooms, id_typeroom FROM rooms WHERE num_room NOT IN (SELECT num_room FROM reserved WHERE (checkin BETWEEN ? AND ? OR checkout BETWEEN ? AND ? OR (checkin <= ? AND checkout >= ?))and status != '5') GROUP BY id_typeroom", [checkin, checkout, checkin, checkout, checkin, checkout], (err, results) => {
                 if (err) throw err;
                 con.query("SELECT roomtype_facility.id,roomtype_facility.room_type_id, roomtype_facility.facility_id ,facility.name , facility.type_id FROM roomtype_facility JOIN facility ON roomtype_facility.facility_id = facility.id JOIN roomstype ON roomstype.id = roomtype_facility.room_type_id", (err, allfacility) => {
                     if (err) throw err;
@@ -61,7 +61,7 @@ module.exports = function (app, con, moment, transporter) {
         const formattedDate = currentDate.format('DD/MM/YYYY HH:mm');
         con.query(" SELECT num_room FROM rooms WHERE id_typeroom = ? AND num_room NOT IN ( SELECT num_room  FROM reserved WHERE id_typeroom = ? AND (checkin BETWEEN ? AND ? OR checkout BETWEEN ? AND ? OR (checkin <= ? AND checkout >= ?))) LIMIT 1 ", [room_type, room_type, checkin, checkout, checkin, checkout, checkin, checkout], (err, num_room) => {
             if (err) throw err
-            var reserved_custom_date = currentDate.format('DDMMYYHHmm')
+            var reserved_custom_date = currentDate.format('DMYYHHmm')
             var reserved_custom_id = 'SF' + cus_id + reserved_custom_date
             con.query("insert into reserved (num_room,id_typeroom,checkin,checkout,cus_id, more_info, payment,reserved_id,status) values (?,?,?,?,?,?,?,?,0) ", [num_room[0].num_room, room_type, checkin, checkout, cus_id, more_info, payment, reserved_custom_id], (err, result) => {
                 if (err) throw err
