4f95c18111b7605f4e760c4e326ffd9ed58db349
FeNixll2 <fiw24018@gmail.com>
2023-06-01 04:29:32 +0700

    อัพเดต แก้ bg lacking footer

--- views/js/component.js
+++ views/js/component.js
@@ -6,20 +6,21 @@ class Footer extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
 
-    <footer class="py-3">
-    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-      <li class="nav-item"><a href="#home" onclick="showSection('home')" class="nav-link px-2 text-muted">หน้าหลัก</a></li>
-      <li class="nav-item"><a href="#rooms" onclick="showSection('rooms')" class="nav-link px-2 text-muted">ห้องพัก</a></li>
-      <li class="nav-item"><a href="#booking" onclick="showSection('booking'),booking_search()" class="nav-link px-2 text-muted">จองห้องพัก</a></li>
-      <li class="nav-item">
-        <a href="#" class="nav-link px-2 text-muted"><i class="fa-brands fa-facebook"></i></a>
-      </li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted"><i class="fa-brands fa-youtube"></i></a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">ติดต่อ 0123456789</a></li>
-
-    </ul>
-    <p class="text-center text-muted">© 2023 โรงแรมบีเอส</p>
-  </footer>
+        <footer class="footer bg-dark text-white text-center mt-auto py-3" style="width: 100vw;">
+        <div class="container">
+            <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                <li class="nav-item"><a href="#home" onclick="showSection('home')" class="nav-link px-2 text-muted">หน้าหลัก</a></li>
+                <li class="nav-item"><a href="#rooms" onclick="showSection('rooms')" class="nav-link px-2 text-muted">ห้องพัก</a></li>
+                <li class="nav-item"><a href="#booking" onclick="showSection('booking'),booking_search()" class="nav-link px-2 text-muted">จองห้องพัก</a></li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link px-2 text-muted"><i class="fa-brands fa-facebook"></i></a>
+                </li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted"><i class="fa-brands fa-youtube"></i></a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">ติดต่อ 0123456789</a></li>
+            </ul>
+            <p class="text-center text-muted">© 2023 โรงแรมบีเอส</p>
+        </div>
+    </footer>
         `;
     }
 }
