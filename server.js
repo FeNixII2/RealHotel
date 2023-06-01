9e75ca0293b7d3ae1ef5796826adfd810e43ffba
FeNixll2 <fiw24018@gmail.com>
2023-06-01 07:31:26 +0700

    อัพ 7โมง

--- server.js
+++ server.js
@@ -7,20 +7,20 @@ const nodemailer = require("nodemailer");
 
 // database connect setup
 const con = mysql.createConnection({
-  host: "25.19.244.218",
-  // host: "localhost",
-  user: "root",
-  password: "",
-  database: "hotel",
-  connectTimeout: 100000,
+    host: "192.168.1.92",
+    // host: "localhost",
+    user: "root",
+    password: "",
+    database: "hotel",
+    connectTimeout: 100000,
 });
 
 con.connect((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("database connected!");
-  }
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("database connected!");
+    }
 });
 
 //app setup
@@ -39,40 +39,40 @@ app.set("view engine", "ejs");
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(
-  session({
-    secret: "keyboard cat",
-    resave: false,
-    saveUninitialized: true,
-  })
+    session({
+        secret: "keyboard cat",
+        resave: false,
+        saveUninitialized: true,
+    })
 );
 
 //port setting to run on the server
 var port = 8080;
 app.listen(port, () => {
-  console.log("web start listening on port  : " + port);
+    console.log("web start listening on port  : " + port);
 });
 
 let transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // true for 465, false for other ports
-  auth: {
-    user: "testinghotel9@gmail.com", // your email
-    pass: "123456789aA!", // your password
-  },
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+        user: "testinghotel9@gmail.com", // your email
+        pass: "123456789aA!", // your password
+    },
 });
 
 app.get("/", (req, res) => {
-  con.query("select * from roomstype", (err, datarooms) => {
-    con.query(
-      "select * from roomstype order by price asc",
-      (err, roomstype) => {
-        con.query("select * from payment", (err, payment_type) => {
-          res.render("mainpage.ejs", { datarooms, roomstype, payment_type });
-        });
-      }
-    );
-  });
+    con.query("select * from roomstype", (err, datarooms) => {
+        con.query(
+            "select * from roomstype order by price asc",
+            (err, roomstype) => {
+                con.query("select * from payment", (err, payment_type) => {
+                    res.render("mainpage.ejs", { datarooms, roomstype, payment_type });
+                });
+            }
+        );
+    });
 });
 //js file include
 
