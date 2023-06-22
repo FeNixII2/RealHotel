2dc666c900bd2ba5de5c2c012d54a8973f47e4ed
MILD <boommild1@gmail.com>
2023-06-22 12:05:24 +0700

    ทำหน้า checkroom ใหม่

--- app/checkbooking.js
+++ app/checkbooking.js
@@ -1,20 +1,24 @@
 module.exports = function (app, con, moment, transporter) {
-    app.post("/checkbooking", (req, res) => {
-        var { idnumcheckbooking } = req.body;
-        var allbooking
-        if (idnumcheckbooking == "") {
-            res.send({ success: false, allbooking })
-            // console.log("not have");
-        } else {
-            // console.log("have");
-            con.query("select * from reserved JOIN customer ON reserved.cus_id = customer.id JOIN roomstype ON roomstype.id = reserved.id_typeroom WHERE ? = customer.p_num OR ? = reserved.reserved_id ORDER BY reserved.`status` asc", [idnumcheckbooking, idnumcheckbooking], (err, allbooking) => {
-                if (err) throw err
-                // console.log(allbooking);
-                res.send({ success: true, allbooking })
+    app.post("/get_booking_code", (req, res) => {
 
-            });
-        }
+    })
 
-    });
+    // app.post("/checkbooking", (req, res) => {
+    //     var { idnumcheckbooking } = req.body;
+    //     var allbooking
+    //     if (idnumcheckbooking == "") {
+    //         res.send({ success: false, allbooking })
+    //         // console.log("not have");
+    //     } else {
+    //         // console.log("have");
+    //         con.query("select * from reserved JOIN customer ON reserved.cus_id = customer.id JOIN roomstype ON roomstype.id = reserved.id_typeroom WHERE ? = customer.p_num OR ? = reserved.reserved_id ORDER BY reserved.`status` asc", [idnumcheckbooking, idnumcheckbooking], (err, allbooking) => {
+    //             if (err) throw err
+    //             // console.log(allbooking);
+    //             res.send({ success: true, allbooking })
+
+    //         });
+    //     }
+
+    // });
 
 };
