290bfcf10642edd38dce0a80cb2ee286d454a47a
Fiwz <fiw24018@gmail.com>
2023-07-01 20:10:12 +0700

    upload

--- app/checkbooking.js
+++ app/checkbooking.js
@@ -11,11 +11,20 @@ module.exports = function (app, con, moment, transporter) {
 
     app.post("/get_reserv_info", (req, res) => {
         var { code, phoneNumber } = req.body
-        con.query(`select reserved.*,customer.*,RIGHT(customer.p_num, 4) AS digit ,roomstype.name,roomstype.name_th,roomstype.price from reserved LEFT JOIN customer ON reserved.cus_id=customer.id LEFT JOIN roomstype ON reserved.id_typeroom = roomstype.id where reserved_id ='${code}' and status in (0,1)  AND RIGHT(p_num, 4) = '${phoneNumber}' `, (err, reserv_info) => {
-
+        con.query(`select reserved.*,customer.*,RIGHT(customer.p_num, 4) AS digit
+        ,roomstype.name,roomstype.name_th,roomstype.price,roomstype.bed,roomstype.size
+        ,roomstype.count_humen,roomstype.extional,payment.pay_type,booking_status.status_name from reserved
+        LEFT JOIN customer ON reserved.cus_id=customer.id
+        LEFT JOIN roomstype ON reserved.id_typeroom = roomstype.id
+        LEFT JOIN payment ON reserved.payment = payment.id
+        LEFT JOIN booking_status ON reserved.status = booking_status.status_id
+         where reserved_id ='${code}'
+        and status in (0,1)  AND RIGHT(p_num, 4) = '${phoneNumber}'`, (err, reserv_info) => {
             if (err) throw err
-            res.send({ reserv_info })
-        })
-    })
-
+            con.query(`SELECT service.name,service.price FROM reserved_service LEFT JOIN service ON reserved_service.service_id=service.id  WHERE reserved_id = '${code}'`, (err, showservice) => {
+                if (err) throw err
+                res.send({ reserv_info, showservice })
+            });
+        });
+    });
 };
